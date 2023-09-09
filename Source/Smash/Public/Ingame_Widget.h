92f8fc7b4f83db214a0c1fda234976617616a153
BanBanjh <banjh9nature12@gmail.com>
2023-09-09 12:48:27 +0900

    Commit BJH 02
    Create Widget

---
+++ Source/Smash/Public/Ingame_Widget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Ingame_Widget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SMASH_API UIngame_Widget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
