067dc7b33f78a221c069cb15add2ce124c62679e
Semira5 <semira5@naver.com>
2023-09-22 12:39:50 +0900

    22Hap

---
+++ Source/Smash/Public/OSY_ScoreWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "OSY_ScoreWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SMASH_API UOSY_ScoreWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
